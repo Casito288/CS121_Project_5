df9f21ed34bf6eaddb1304645167932e3fc0330e
Casito288 <ethancastro0805@proton.me>
2026-09-25 16:51:14 -0400

    Add files via upload

---
+++ Game/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "race.h"
+#include "horse.h"
+
+int main(){
+  Race();
+  Horse();
+  return 0;
+}
---
+++ Game/race.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+class Race {
+
+  private:
+    const static int NUM_HORSES = 5;
+    const static int  TRACK_LENGTH = 15;
+  public:
+    int horses[NUM_HORSES];
+    Race();
+    void start();
+
+};
